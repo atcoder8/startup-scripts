5d1c09cea38c8c4b5be11b10185acdbd07c26e68
atcoder8 <atcoder8@gmail.com>
2023-05-24 11:17:52 +0900

    Added function `update`.

--- functions.sh
+++ functions.sh
@@ -1,3 +1,33 @@
+function update() {
+    echo "# Execute automatic upgrades."
+
+    # Upgrade apt packages.
+    echo -e "\n# Execute \`sudo apt update\`."
+    sudo apt update
+    echo -e "\n# Execute \`sudo apt -y upgrade\`."
+    sudo apt -y upgrade
+
+    # Clean up.
+    echo -e "\n# Execute \`sudo apt -y autoremove\`."
+    sudo apt -y autoremove
+    echo -e "\n# Execute \`sudo apt autoclean\`."
+    sudo apt autoclean
+    echo -e "\n# Execute \`sudo apt autopurge\`."
+    sudo apt autopurge
+
+    # Upgrade pyenv.
+    if [ -v PYENV_ROOT ]; then
+    echo -e "\n# Execute \`git -C ${PYENV_ROOT} pull\`."
+    git -C ${PYENV_ROOT} pull
+    fi
+
+    # Update Rust toolchains and rustup.
+    if [ -d "$HOME/.rustup" ]; then
+    echo -e "\n# Execute \`rustup update\`."
+    rustup update
+    fi
+}
+
 function ac_new() {
     if [ $# -ne 1 ]; then
         echo "Usage: ac_new <CONTEST NAME>"
