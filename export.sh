b52b56c5916e89df0d4a47236ad91e6dda51545e
pencoder <atcoder8@gmail.com>
2024-09-15 17:22:22 +0900

    Added `$HOME/.local/bin` into `PATH`.

--- export.sh
+++ export.sh
@@ -2,6 +2,10 @@ if [ -d "$HOME/bin" ]; then
     export PATH="$HOME/bin:$PATH"
 fi
 
+if [ -d "$HOME/.local/bin" ]; then
+    export PATH="$HOME/.local/bin:$PATH"
+fi
+
 if [ -d "$HOME/app/android-studio/bin" ]; then
     export PATH="$HOME/app/android-studio/bin:$PATH"
 fi
